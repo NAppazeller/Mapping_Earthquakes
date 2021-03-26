34b0f324b78a936cf959be7c67b919b681564acf
unknown <you@example.com>
2021-03-26 15:43:26 -0500

    multi-point map progress

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -13,6 +13,9 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
  //}).addTo(map);
 
  //  Add a circle to the map for Los Angeles, California.
+//let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+//  Add a marker to the map for Los Angeles, California.
 L.circleMarker([34.0522, -118.2437], {
     radius: 300,
     color: "black",
